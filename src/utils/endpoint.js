71a57fba78813dffc50a5e0319b154f41a95ad3c
Carlo Martinucci <carlo.martinucci@gmail.com>
2019-06-02 10:27:44 +0200

    Use different endpoints depending on env variables

--- src/utils/endpoint.js
+++ src/utils/endpoint.js
@@ -1,5 +1,10 @@
-const { protocol, hostname } = document.location;
-const uri = `${protocol}//${hostname}:3000/`;
+let uri
+if (process.env.NODE_ENV === 'development') {
+  const { protocol, hostname } = document.location;
+  uri = `${protocol}//${hostname}:3000/`;
+} else {
+  uri = `https://api-listadellaspesa.herokuapp.com/`
+}
 
 const endpoint = path => `${uri}${path}`;
 
